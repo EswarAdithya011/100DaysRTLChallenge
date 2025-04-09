174ea693f32b91c7a5f375921ae4f76a505d15ec
Korrapolu Eswar Adithya <141947460+EswarAdithya011@users.noreply.github.com>
2025-04-09 22:14:53 +0530

    Add files via upload

---
+++ Linear Feedback Shift Register (4-bit)/DFlipFlop.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Korrapolu Eswar Adithya
+// 
+// Create Date: 13.02.2025 10:38:37
+// Design Name: 
+// Module Name: DFlipFlop
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module DFlipFlop(output reg Q, output Qb, input D, clk, clr, pre);
+    
+    always @(posedge clk or posedge clr or posedge pre)  
+        if (clr)  
+            Q <= 1'b1;  
+        else if (pre)  
+            Q <= 1'b1;  
+        else  
+            Q <= D;  
+    assign Qb = ~Q;
+    
+endmodule
+
+
